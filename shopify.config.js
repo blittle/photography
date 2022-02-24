bcd656326d195f210d1da426be483fe79e5aa20f
Bret Little <bret.little@shopify.com>
2022-02-24 13:42:56 -0500

    Update to 0.11.0

--- shopify.config.js
+++ shopify.config.js
@@ -2,5 +2,5 @@ export default {
   defaultLocale: 'en',
   storeDomain: 'bretlittle.myshopify.com',
   storefrontToken: 'e2cb814f19d4ffaa9ca61abecd7917cc',
-  storefrontApiVersion: 'unstable',
+  storefrontApiVersion: '2022-01',
 };
